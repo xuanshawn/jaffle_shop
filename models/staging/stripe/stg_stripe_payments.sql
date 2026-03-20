d35efacb5a00c9686c1bf0f3d004c18cbb7b4d91
xuanshawn <daixchn@gmail.com>
2026-03-20 17:06:16 -0500

    created stg_stripe_payments.sql

---
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -0,0 +1,32 @@
+with
+
+source as (
+
+    -- {# This references seed (CSV) data - try switching to {{ source('ecom', 'raw_stores') }} #}
+    select * from dbt-tutorial.stripe.payment
+
+),
+
+renamed as (
+
+    select
+
+        ----------  ids
+        id as payment_id,
+        orderid as order_id,
+
+        ---------- text
+        paymentmethod as payment_method,
+        status,
+
+        ---------- numerics
+       amount,
+        ---------- timestamps
+        created,
+        _batched_at as batched_at
+
+    from source
+
+)
+
+select * from renamed
