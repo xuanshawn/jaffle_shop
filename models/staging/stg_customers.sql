8963e8ed3768ac88595ec7fbd15fb5e18bd7a02b
xuanshawn <daixchn@gmail.com>
2026-03-23 16:27:20 -0500

    change reference from seed to source macro

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -3,7 +3,8 @@ with
 source as (
 
     -- {# This references seed (CSV) data - try switching to {{ source('ecom', 'raw_customers') }} #}
-    select * from {{ ref('raw_customers') }}
+    -- select * from {{ ref('raw_customers') }}
+    select * from {{ source('ecom', 'raw_customers') }}
 
 ),
 
