d544f80d4b49b4ca41fd22aeaa41f8fb45d7c9f3
xuanshawn <daixchn@gmail.com>
2026-04-01 13:52:56 -0500

    run incremental model with parent(source) change

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,4 +1,6 @@
--- {{ config(materialized='incremental') }}
+{{ config(materialized="incremental",
+unique_key="order_id",
+incremental_strategy="merge") }}
 
 
 
@@ -19,18 +21,21 @@ payments as (
 
     select * from {{ ref('stg_stripe_payments') }}
 
-),
-final as (
-select c.customer_id, o.order_id, o.order_date, sum(p.amount) as amount
-from customers c 
-join orders o on c.customer_id = o.customer_id
-join payments p on o.order_id = p.order_id
-group by 1,2,3
 )
-select * from final
+-- ,
+-- final as (
+-- select c.customer_id, o.order_id, o.order_date, sum(p.amount) as amount
+-- from orders o 
+-- left outer join customers c on c.customer_id = o.customer_id
+-- left outer join payments p on o.order_id = p.order_id
+-- group by 1,2,3
+-- )
+-- select * from final
 
 
 -- select * from final
--- {% if is_incremental() %}
--- where order_date > (select max(order_date) from {{ this }})
--- {% endif %}
+select * from orders
+{% if is_incremental() %}
+where order_date > (select max(order_date) from {{ this }})
+{% endif %}
+order by order_date desc
